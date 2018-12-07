ea95717b76c1cd775ce295e7308be036ffc801e0
Tim Mattison <tim@mattison.org>
2018-12-07 10:34:25 -0500

    Iterating through a fixed list of locales for mosh

--- functions/CDDMoshJava/src/main/java/com/amazonaws/greengrass/cddmosh/handlers/StartNewMoshSessionEventHandler.java
+++ functions/CDDMoshJava/src/main/java/com/amazonaws/greengrass/cddmosh/handlers/StartNewMoshSessionEventHandler.java
@@ -30,11 +30,12 @@ public class StartNewMoshSessionEventHandler implements GreengrassLambdaEventHan
     public static final Pattern MOSH_CONNECT_REGEX_PATTERN = Pattern.compile(MOSH_CONNECT_REGEX);
     public static final String LOCALE_EN_US_UTF_8 = "en_US.UTF-8";
     public static final String LOCALE_EN_GB_UTF_8 = "en_GB.UTF-8";
-    public static final String LOCALE = LOCALE_EN_US_UTF_8;
+    public static final String[] LOCALES = {LOCALE_EN_US_UTF_8, LOCALE_EN_GB_UTF_8};
     private final NativeProcessHelper nativeProcessHelper;
     private final EventBus eventBus;
     private final Topics topics;
     private Optional<Vertx> vertx = Optional.empty();
+    private Optional<String> LOCALE = Optional.empty();
 
     @Override
     public boolean isTopicExpected(String topic) {
@@ -61,64 +62,79 @@ public class StartNewMoshSessionEventHandler implements GreengrassLambdaEventHan
                     VertxOptions vertxOptions = new VertxOptions();
                     vertx = Optional.ofNullable(Vertx.vertx(vertxOptions));
                 }
-            }
 
-            List<String> stdoutLines = new ArrayList<>();
-            List<String> stderrLines = new ArrayList<>();
-
-            Consumer<String> stdoutConsumer = (line) -> stdoutLines.add(line);
-            Consumer<String> stderrConsumer = (line) -> stderrLines.add(line);
-
-            Map<String, String> environment = new HashMap<>();
-            environment.put("LC_CTYPE", LOCALE);
-
-            List<String> programAndArguments = new ArrayList<>();
-            programAndArguments.add("/usr/bin/mosh-server");
-
-            ProcessBuilder pb = new ProcessBuilder(programAndArguments);
-
-            pb.environment().putAll(environment);
-
-            nativeProcessHelper.getOutputFromProcess(log, pb, true, Optional.of(stdoutConsumer), Optional.of(stderrConsumer));
-
-            Optional<String> result = stdoutLines.stream()
-                    .filter(line -> line.matches(MOSH_CONNECT_REGEX))
-                    .findFirst();
-
-            int port;
-            String key;
-
-            if (result.isPresent()) {
-                Matcher matcher = MOSH_CONNECT_REGEX_PATTERN.matcher(result.get());
-                matcher.find();
-                port = Integer.parseInt(matcher.group(1));
-                key = matcher.group(2);
-
-                eventBus.post(PublishObjectEvent.builder().topic(topics.getOutputTopic()).object(KeyAndPort.builder().key(key).port(port).build()).build());
-
-                int listenPort = port + 4096;
-                log.info("Starting server on [" + listenPort + "]");
-                DatagramSocket datagramSocket = vertx.get().createDatagramSocket();
-                // Take any inbound data and turn it into an event
-                datagramSocket.handler(event -> {
-//                    log.info("Sending data back");
-                    eventBus.post(OutboundDataEvent.builder().port(event.sender().port()).data(event.data().getBytes()).build());
-                });
-                datagramSocket.listen(listenPort, "127.0.0.1", s -> {
-                });
-                log.info("Server started on [" + listenPort + "]");
-                eventBus.post(DatagramSocketCreatedEvent.builder().datagramSocket(datagramSocket).sendPort(port).listenPort(listenPort).build());
-            } else {
-                log.error("START mosh failed to start");
-                log.error("stdout:");
-                log.error(String.join("\n", stdoutLines));
-                log.error("stderr:");
-                log.error(String.join("\n", stderrLines));
-                log.error("END   mosh failed to start");
+                if (LOCALE.isPresent()) {
+                    launchWithLocale(LOCALE.get());
+                } else {
+                    // Find the first locale that works and save it if we find one
+                    LOCALE = Arrays.stream(LOCALES)
+                            .map(this::launchWithLocale)
+                            .filter(Objects::nonNull)
+                            .findFirst();
+                }
             }
         } catch (Exception e) {
             log.info("Exception [" + e.getMessage() + "]");
         }
+    }
+
+    private String launchWithLocale(String locale) {
+        List<String> stdoutLines = new ArrayList<>();
+        List<String> stderrLines = new ArrayList<>();
+
+        Consumer<String> stdoutConsumer = (line) -> stdoutLines.add(line);
+        Consumer<String> stderrConsumer = (line) -> stderrLines.add(line);
+
+        Map<String, String> environment = new HashMap<>();
+        environment.put("LC_CTYPE", locale);
+
+        List<String> programAndArguments = new ArrayList<>();
+        programAndArguments.add("/usr/bin/mosh-server");
+
+        ProcessBuilder pb = new ProcessBuilder(programAndArguments);
+
+        pb.environment().putAll(environment);
+
+        nativeProcessHelper.getOutputFromProcess(log, pb, true, Optional.of(stdoutConsumer), Optional.of(stderrConsumer));
+
+        Optional<String> result = stdoutLines.stream()
+                .filter(line -> line.matches(MOSH_CONNECT_REGEX))
+                .findFirst();
+
+        int port;
+        String key;
+
+        if (!result.isPresent()) {
+            log.error("START mosh failed to start");
+            log.error("stdout:");
+            log.error(String.join("\n", stdoutLines));
+            log.error("stderr:");
+            log.error(String.join("\n", stderrLines));
+            log.error("END   mosh failed to start");
+
+            return null;
+        }
+
+        Matcher matcher = MOSH_CONNECT_REGEX_PATTERN.matcher(result.get());
+        matcher.find();
+        port = Integer.parseInt(matcher.group(1));
+        key = matcher.group(2);
+
+        eventBus.post(PublishObjectEvent.builder().topic(topics.getOutputTopic()).object(KeyAndPort.builder().key(key).port(port).build()).build());
+
+        int listenPort = port + 4096;
+        log.info("Starting server on [" + listenPort + "]");
+        DatagramSocket datagramSocket = vertx.get().createDatagramSocket();
+        // Take any inbound data and turn it into an event
+        datagramSocket.handler(event -> {
+            eventBus.post(OutboundDataEvent.builder().port(event.sender().port()).data(event.data().getBytes()).build());
+        });
+        datagramSocket.listen(listenPort, "127.0.0.1", s -> {
+        });
+        log.info("Server started on [" + listenPort + "]");
+        eventBus.post(DatagramSocketCreatedEvent.builder().datagramSocket(datagramSocket).sendPort(port).listenPort(listenPort).build());
+
+        return result.get();
     }
 
     @Subscribe
